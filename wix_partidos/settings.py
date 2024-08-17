3131f6f9e7e9f0bf2baaff9ccc63836a36c71926
franciscolvg <franciscolvg04@gmail.com>
2024-08-16 20:21:16 -0400

    Actualiza ALLOWED_HOSTS en settings.py para permitir acceso desde PythonAnywhere

--- wix_partidos/settings.py
+++ wix_partidos/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kw&)yq1dt*##^cgsk5+04l+(+1$n5tw=@28(b=9#%--nd^x&m5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['francisco04.pythonanywhere.com']
 
 
 # Application definition
